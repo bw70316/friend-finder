c9c0b945a6b207afe63a6939f9dcb1e1d2bec9d2
brian wisnowski <brian wisnowski>
2017-05-18 14:09:52 -0400

    when i add the modal my objects stop pushing to the api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,52 @@
 var friendsData = require('../data/friends.js');
 
+var path = require('path');
+
+var totalDifference = 0;
+
 module.exports = function (app) {
 
 	app.get('/api/friends', function (req, res) {
 		res.json(friendsData);
 
 	});
-}
+
+	app.post('api/friends', function (req, res) {
+
+		var greatMatch = {
+			name: "",
+			photo: "",
+			matchDifference: 40
+		};
+
+		var userData = req.body;
+		var userName= userData.name;
+		var userPhoto = user.photo;
+		var userScores = userData.scores;
+
+		var totalDifference = 0;
+
+		  for(var i = 0; i < [friendsData].length-1; i++) {
+			console.log(friendsData[i].name);
+			totalDifference=0;
+
+
+		   for(var j = 0; j < 5; j++) {
+
+			totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+
+			if (totalDifference <= greatMatch.friendDifference) {
+
+				greatMatch.name= friendsData[i].name
+				greatMatch.photo = friendsData[i].photo;
+				greatMatch.matchDifference = totalDifference;
+			}
+		}
+		
+
+	}
+			friendsData.push(userData);
+
+		res.json(greatMatch);
+	});
+};
