121f1e560cbce59182f6fa8b6a1b3b1f738b4c64
brian wisnowski <brian wisnowski>
2017-05-18 20:44:51 -0400

    trying heroku again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
  
 
-require('./app/routing/apiroutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function(){
